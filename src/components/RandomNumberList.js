9096e5e28f02c06e5ec4a331ca9589c1d0211409
Wojciech Skarbek <woj.skarbek@gmail.com>
2021-01-12 17:47:12 +0100

    Zad 2

--- src/components/RandomNumberList.js
+++ src/components/RandomNumberList.js
@@ -4,10 +4,26 @@ import RandomNumber from "./RandomNumber";
 
 
 export default function RandomNumberList({size, min, max}) {
-    let randomNumbers = [size];
-    for(let i = 0; i < size; i++) {
-        randomNumbers[i] = RandomNumber(min, max);
+
+    const [randomNumbersList, setRandomNumbersList] = React.useState([size]);
+
+
+    React.useEffect(() => {
+        generateRandomNumbers();
+    },[]);
+
+    function generateRandomNumbers() {
+        let randomNumbers = [size];
+        for(let i = 0; i < size; i++) {
+            randomNumbers[i] = RandomNumber(min, max);
+        }
+        setRandomNumbersList(randomNumbers);
     }
-    console.log(randomNumbers);
-    return<List objs={randomNumbers}/>
+
+    return (
+        <div className="RandomNumberList">
+            <List objs={randomNumbersList}/>
+            <button onClick={generateRandomNumbers}>Wczytaj nowe liczby</button>
+        </div>
+    )
 }
