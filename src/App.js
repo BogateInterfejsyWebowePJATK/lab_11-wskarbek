c6d39d4e83649bb3d0cf09a9a5e8343e1cb8f06b
Wojciech Skarbek <woj.skarbek@gmail.com>
2021-01-12 17:50:57 +0100

    List.js added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import RandomNumberList from "./components/RandomNumberList";
 function App() {
   return (
     <div className="App">
-        <div className="RandomNumberList">
-            <RandomNumberList size={10} min={1} max={10}/>
-        </div>
+        <RandomNumberList size={10} min={1} max={10}/>
     </div>
   );
 }
---
+++ src/components/List.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function List({objs}){
+    return (
+        <div className="List">
+            <ul>
+                {objs.map((obj, key) => {
+                    return (
+                        <li key={key}>
+                            {obj}
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    );
+}
